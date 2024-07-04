a52b81823e26ec7bc0fc3e131999a40dfaa9c3fd
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-07-04 01:06:34 -0400

    Switch back to eclipse-temurin:11-jdk-focal.

--- tools/scripts/ubuntu-focal/install-deps-and-build-native-lib.sh
+++ tools/scripts/ubuntu-focal/install-deps-and-build-native-lib.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential cmake maven openjdk-11-jdk
+DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential cmake maven
 
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-*
 mvn --batch-mode generate-resources validate assembly:single@assemble-lib-dir
