7b667c78bba5a0eee331e921c7449637dfed6527
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-07-03 23:08:30 -0400

    Set JAVA_HOME

--- tools/scripts/ubuntu-focal/install-deps-and-build-native-lib.sh
+++ tools/scripts/ubuntu-focal/install-deps-and-build-native-lib.sh
@@ -3,4 +3,5 @@
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential cmake maven openjdk-11-jdk
 
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-*
 mvn --batch-mode generate-resources validate assembly:single@assemble-lib-dir
