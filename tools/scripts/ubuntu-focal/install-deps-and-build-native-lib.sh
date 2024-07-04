aa1fe3c75d5bfed72176eb878a82218d3507b030
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-07-03 22:52:41 -0400

    Use ubuntu:20.04 to build ARM native-lib.

--- tools/scripts/ubuntu-focal/install-deps-and-build-native-lib.sh
+++ tools/scripts/ubuntu-focal/install-deps-and-build-native-lib.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y build-essential cmake maven
+apt-get install -y build-essential cmake maven openjdk-11-jdk
 
 mvn --batch-mode generate-resources validate assembly:single@assemble-lib-dir
