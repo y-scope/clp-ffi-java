42a140590a8ce06eacd92ecb831f6c38175c40d9
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-07-04 01:09:42 -0400

    Make script more robust.

--- tools/scripts/ubuntu-focal/install-deps-and-build-native-lib.sh
+++ tools/scripts/ubuntu-focal/install-deps-and-build-native-lib.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+# Exit on error
+set -e
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+project_dir="${script_dir}/../../../"
+
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential cmake maven
 
+cd "$project_dir"
 mvn --batch-mode generate-resources validate assembly:single@assemble-lib-dir
