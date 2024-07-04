63bd3cc3cd4c6cf3db492f3919749e22309bc072
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-07-03 22:57:40 -0400

    Use DEBIAN_FRONTEND=noninteractive to handle jdk install.

--- tools/scripts/ubuntu-focal/install-deps-and-build-native-lib.sh
+++ tools/scripts/ubuntu-focal/install-deps-and-build-native-lib.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y build-essential cmake maven openjdk-11-jdk
+DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential cmake maven openjdk-11-jdk
 
 mvn --batch-mode generate-resources validate assembly:single@assemble-lib-dir
