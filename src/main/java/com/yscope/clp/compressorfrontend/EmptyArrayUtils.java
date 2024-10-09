57490685095ab39e010015070d4d33f98d209d48
Jack Luo <jluo14@ext.uber.com>
2024-10-10 00:55:13 +0800

    Added empty byte[][] to EmptyArrayUtils

--- src/main/java/com/yscope/clp/compressorfrontend/EmptyArrayUtils.java
+++ src/main/java/com/yscope/clp/compressorfrontend/EmptyArrayUtils.java
@@ -8,6 +8,7 @@ public class EmptyArrayUtils {
   public static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
   public static final int[] EMPTY_INT_ARRAY = new int[0];
   public static final long[] EMPTY_LONG_ARRAY = new long[0];
+  public static final byte[][] EMPTY_BYTE_ARRAY_ARRAY = new byte[0][];
   public static final FlattenedByteArray EMPTY_FLATTENED_BYTE_ARRAY =
       new FlattenedByteArray(EMPTY_BYTE_ARRAY, EMPTY_INT_ARRAY);
 
@@ -21,6 +22,10 @@ public class EmptyArrayUtils {
 
   public static long[] getNonNullLongArray(long[] longArray) {
     return (null == longArray) ? EMPTY_LONG_ARRAY : longArray;
+  }
+
+  public static byte[][] getEmptyByteArrayArray(byte[][] byteArrayArray) {
+    return (null == byteArrayArray) ? EMPTY_BYTE_ARRAY_ARRAY : byteArrayArray;
   }
 
   public static FlattenedByteArray getNonNullFlattenedByteArray(
