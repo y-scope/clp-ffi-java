y-scope/clp-ffi-java

commit b4a38977dbc433906cbeb1754b9df1b9a86d9042

Add support for getting `EncodedMessage`'s dictionary vars as byte arrays. (#53)
Co-authored-by: Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>

src/main/java/com/yscope/clp/compressorfrontend/EmptyArrayUtils.java
@@ -8,6 +8,7 @@ public class EmptyArrayUtils {
   public static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
   public static final int[] EMPTY_INT_ARRAY = new int[0];
   public static final long[] EMPTY_LONG_ARRAY = new long[0];
+  public static final byte[][] EMPTY_BYTE_ARRAY_ARRAY = new byte[0][];
   public static final FlattenedByteArray EMPTY_FLATTENED_BYTE_ARRAY =
       new FlattenedByteArray(EMPTY_BYTE_ARRAY, EMPTY_INT_ARRAY);
 
@@ -21,6 +22,10 @@ public class EmptyArrayUtils {
 
   public static long[] getNonNullLongArray(long[] longArray) {
     return (null == longArray) ? EMPTY_LONG_ARRAY : longArray;
+  }
+
+  public static byte[][] getEmptyByteArrayArray(byte[][] byteArrayArray) {
+    return (null == byteArrayArray) ? EMPTY_BYTE_ARRAY_ARRAY : byteArrayArray;
   }
 
   public static FlattenedByteArray getNonNullFlattenedByteArray(

src/main/java/com/yscope/clp/compressorfrontend/EncodedMessage.java
@@ -1,6 +1,7 @@
 package com.yscope.clp.compressorfrontend;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.stream.LongStream;
 
@@ -63,6 +64,24 @@ public class EncodedMessage {
       int endOffset = dictionaryVarBounds[j++];
       dictVars[i] = new String(message, beginOffset,
           endOffset - beginOffset, StandardCharsets.ISO_8859_1);
+    }
+    return dictVars;
+  }
+
+  /**
+   * @return The dictionary variables instantiated as {@code byte[]}s.
+   */
+  public byte[][] getDictionaryVarsAsByteArrays() {
+    if (null == dictionaryVarBounds) {
+      return null;
+    }
+
+    int numVars = dictionaryVarBounds.length / 2;
+    byte[][] dictVars = new byte[numVars][];
+    for (int i = 0, j = 0; i < dictVars.length; ++i) {
+      int beginOffset = dictionaryVarBounds[j++];
+      int endOffset = dictionaryVarBounds[j++];
+      dictVars[i] = Arrays.copyOfRange(message, beginOffset, endOffset);
     }
     return dictVars;
   }

src/main/java/com/yscope/clp/compressorfrontend/FlattenedByteArrayFactory.java
@@ -7,6 +7,35 @@ public class FlattenedByteArrayFactory {
     private static ThreadLocal<ByteArrayOutputStream> reusableByteArrayOutputStream;
 
     /**
+     * Constructs a FlattenedByteArray by concatenating the given byte arrays together.
+     * @param byteArrays
+     * @return The flattened array.
+     */
+    public static FlattenedByteArray fromByteArrays(byte[][] byteArrays) {
+        if (null == byteArrays || 0 == byteArrays.length) {
+            return EmptyArrayUtils.EMPTY_FLATTENED_BYTE_ARRAY;
+        }
+
+        // Compute flattenedElemsEndOffsets
+        int flattenedElemsLength = 0;
+        int[] flattenedElemsEndOffsets = new int[byteArrays.length];
+        for (int i = 0; i < byteArrays.length; i++) {
+            flattenedElemsLength += byteArrays[i].length;
+            flattenedElemsEndOffsets[i] = flattenedElemsLength;
+        }
+
+        // Compute flattenedElems
+        byte[] flattenedElems = new byte[flattenedElemsLength];
+        for (int i = 0, flattenedElemsOffset = 0; i < byteArrays.length; i++) {
+            byte[] byteArray = byteArrays[i];
+            System.arraycopy(byteArray, 0, flattenedElems, flattenedElemsOffset, byteArray.length);
+            flattenedElemsOffset += byteArray.length;
+        }
+
+        return new FlattenedByteArray(flattenedElems, flattenedElemsEndOffsets);
+    }
+
+    /**
      * Constructs a FlattenedByteArray by converting the given strings into byte arrays and then
      * concatenating them together.
      * @param strings

src/test/java/com/yscope/clp/compressorfrontend/EncodingTests.java
@@ -82,6 +82,13 @@ public class EncodingTests {
     // Test decoding from byte arrays
     decodedMessage = messageDecoder.decodeMessage(
         encodedMessage.getLogtype(),
+        FlattenedByteArrayFactory.fromByteArrays(encodedMessage.getDictionaryVarsAsByteArrays()),
+        encodedMessage.getEncodedVars()
+    );
+    assertEquals(originalMessage, decodedMessage);
+
+    decodedMessage = messageDecoder.decodeMessage(
+        encodedMessage.getLogtype(),
         encodedMessage.getDictionaryVarsAsFlattenedByteArray(),
         encodedMessage.getEncodedVars()
     );

commit ef4cb5715f339befc180e690d8b381eb2aee3e77

Fix incorrect name in `EmptyArrayUtils` introduced in #53. (#54)

src/main/java/com/yscope/clp/compressorfrontend/EmptyArrayUtils.java
@@ -24,7 +24,7 @@ public class EmptyArrayUtils {
     return (null == longArray) ? EMPTY_LONG_ARRAY : longArray;
   }
 
-  public static byte[][] getEmptyByteArrayArray(byte[][] byteArrayArray) {
+  public static byte[][] getNonNullByteArrayArray(byte[][] byteArrayArray) {
     return (null == byteArrayArray) ? EMPTY_BYTE_ARRAY_ARRAY : byteArrayArray;
   }
 